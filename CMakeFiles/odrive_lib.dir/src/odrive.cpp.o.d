15494ac0d0ba486c1149a8847f87ce60e002585f
Daniel C Schad <daniel@schad.se>
2022-09-20 15:37:36 +0200

    Remove ignored files

--- CMakeFiles/odrive_lib.dir/src/odrive.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/odrive_lib.dir/src/odrive.cpp.o: \
-  /Users/slappenrobotfri/Documents/GitHub/odrive-cpp/src/odrive.cpp \
-  /Users/slappenrobotfri/Documents/GitHub/odrive-cpp/include/odrive.h
