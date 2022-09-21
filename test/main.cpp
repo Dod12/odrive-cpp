a1fa681aff7ac7321b58b1a0c9f4c8f2e375e7a4
Daniel Carlström Schad <da8454sh-s@student.lu.se>
2022-09-21 16:03:21 +0200

    Update main to prevent segfault

--- test/main.cpp
+++ test/main.cpp
@@ -9,7 +9,15 @@
 int main(int argc, char* argv[]){
     auto* odrive = new odrive::ODriveUSB();
     int64_t serial = 35632015560013;
-    odrive->init(std::vector<std::vector<int64_t>>{{serial}});
+    odrive->init(std::vector<std::vector<int64_t>>{{serial}, {serial}});
     int target_pos = 1;
-    odrive->write(serial, odrive::AXIS__CONTROLLER__INPUT_POS, target_pos);
+    float vbus_voltage;
+    try {
+        odrive->read(serial, odrive::VBUS_VOLTAGE, vbus_voltage);
+        std::cout << "Vbus voltage: " << vbus_voltage << std::endl;
+    } catch (std::exception& e) {
+        std::cout << "Error reading VBUS voltage" << std::endl;
+        return 1;
+    }
+    //odrive->write(serial, odrive::AXIS__CONTROLLER__INPUT_POS, target_pos);
 }
