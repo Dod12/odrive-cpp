aca4b12ff29e535d2972d2dbe6fb085a51239547
Daniel Carlström Schad <da8454sh-s@student.lu.se>
2022-10-03 17:42:33 +0200

    Update odrive_config.py

--- test/odrive_config.py
+++ test/odrive_config.py
@@ -12,7 +12,7 @@ if device is not None:
 else:
     raise IOError("Could not find ODrive")
 
-control_mode = "bandwidth"  # Control mode for filtering target position. Must be one of [None, "bandwidth", "trap"].
+control_mode = "none"  # Control mode for filtering target position. Must be one of [None, "bandwidth", "trap"].
 tick_rate = 2
 
 # Setup global settings
@@ -22,13 +22,14 @@ device.config.dc_max_negative_current = -0.001
 
 # Setup per-axis settings
 for axis in axes:
-    axis.motor.config.current_lim = 15
+    axis.motor.config.current_lim = 5
     axis.controller.config.vel_limit = 10
     axis.motor.config.calibration_current = 10
     axis.motor.config.pole_pairs = 7
     axis.motor.config.torque_constant = 8.27 / 150
     axis.motor.config.motor_type = enums.MOTOR_TYPE_HIGH_CURRENT
     axis.encoder.config.cpr = 8192
+    # axis.encoder.config.direction = 1 # Invert control direction for left encoder position.
     if control_mode == "trap":
         axis.trap_traj.config.vel_limit = 5
         axis.trap_traj.config.accel_limit = 1
@@ -38,6 +39,8 @@ for axis in axes:
     elif control_mode == "bandwidth":
         axis.controller.config.input_filter_bandwidth = tick_rate / 2  # Set to half the position update frequency.
         axis.controller.config.input_mode = enums.INPUT_MODE_POS_FILTER
+    else:
+        axis.controller.config.input_mode = enums.INPUT_MODE_PASSTHROUGH
     axis.config.startup_motor_calibration = True
     axis.config.startup_encoder_index_search = True
     axis.config.startup_encoder_offset_calibration = True
