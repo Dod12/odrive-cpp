0c3f0d3b8018d47ae2f7c6af084be21834ea18c3
Daniel Carlström Schad <da8454sh-s@student.lu.se>
2022-12-06 16:25:31 +0100

    Update odrive_config.py

--- test/odrive_config.py
+++ test/odrive_config.py
@@ -2,15 +2,15 @@ import time
 
 import odrive
 import odrive.enums as enums
-from odrive import fibre
+#from odrive import fibre
 
 device = odrive.find_any()
-axes = [device.axis0, device.axis1]
 
-if device is not None:
-    print("Found ODrive", device.serial_number)
-else:
+if device is None:
     raise IOError("Could not find ODrive")
+
+print("Found ODrive", device.serial_number)
+axes = [device.axis0, device.axis1]
 
 control_mode = "none"  # Control mode for filtering target position. Must be one of [None, "bandwidth", "trap"].
 tick_rate = 2
@@ -22,8 +22,10 @@ device.config.dc_max_negative_current = -0.001
 
 # Setup per-axis settings
 for axis in axes:
-    axis.motor.config.current_lim = 5
-    axis.controller.config.vel_limit = 10
+    axis.motor.config.current_lim = 10
+    axis.controller.config.vel_limit = 15
+    #axis.controller.config.vel_gain = 1.493466854095459
+    #axis.controller.config.pos_gain = 132.88357543945312
     axis.motor.config.calibration_current = 10
     axis.motor.config.pole_pairs = 7
     axis.motor.config.torque_constant = 8.27 / 150
@@ -41,16 +43,21 @@ for axis in axes:
         axis.controller.config.input_mode = enums.INPUT_MODE_POS_FILTER
     else:
         axis.controller.config.input_mode = enums.INPUT_MODE_PASSTHROUGH
-    axis.config.startup_motor_calibration = True
+
+    # Startup calibration
+    axis.motor.config.pre_calibrated = False
+    axis.encoder.config.pre_calibrated = False
+
     axis.config.startup_encoder_index_search = True
-    axis.config.startup_encoder_offset_calibration = True
     axis.config.startup_closed_loop_control = True
+    axis.config.startup_encoder_offset_calibration = True
+    axis.config.startup_motor_calibration = True
 
 for axis in axes:
     axis.requested_state = enums.AXIS_STATE_IDLE
 
 try:
     device.save_configuration()
-except fibre.libfibre.ObjectLostError:
+except:
     print("Rebooting...")
     pass
